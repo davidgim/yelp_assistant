d5bd8defe9bef66d8e9765cf3c6ae86f6adbae5c
David <davidgim@gmail.com>
2025-02-28 11:03:28 -0800

    Final fixes to deployment / removed uncessary API key

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,9 +5,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
 
-   
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') 
-
     GROQ_API_KEY = os.getenv('GROQ_API_KEY')
     DEBUG = os.getenv('FLASK_DEBUG', True)
 
